f99f28df22a0065b5d8ac3df670a89ec3539523e
wanggeng.wg <wanggeng.wg@alibaba-inc.com>
2021-10-09 19:07:07 +0800

    前台

---
+++ bjtu-fe-assets/src/pages/TaskList/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Image, List } from 'antd-mobile';
+
+class TaskList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <List>
+        <List.Item
+          extra="次要信息"
+          title="这里是标题"
+          description="这里是描述信息"
+          clickable
+        >
+          这里是主信息
+        </List.Item>
+        <List.Item title="这里是标题" clickable>
+          这里是主信息
+        </List.Item>
+        <List.Item title="这里是标题">这里是主信息</List.Item>
+      </List>
+    );
+  }
+}
+
+export default TaskList;
